2578bb9c382bdbaa8b36914ffe498c0d2a6aa076
Yubi Mamiya <yubimamiya@gmail.com>
2026-09-18 14:48:32 -0400

    add mapbox token

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 // TODO: Insert your Mapbox token here
-mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 const Map = () => {
   const mapContainer = useRef(null);
