a543b66c4fa1c18a59d2d3b52b0b643b4413de76
bhoomipatni <bpatni2020@gmail.com>
2026-09-18 14:55:12 -0400

    Configure Mapbox token from environment

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-// TODO: Insert your Mapbox token here
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 const Map = () => {
